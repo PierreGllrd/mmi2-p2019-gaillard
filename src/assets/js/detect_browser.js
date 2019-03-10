f0fb7981010cac2f217cb7646a2ac023cac4c9ca
PierreGllrd <gaillard.pierre1999@gmail.com>
2019-03-10 21:56:25 +0100

    Repair Browsers

--- src/assets/js/detect_browser.js
+++ src/assets/js/detect_browser.js
@@ -3,14 +3,13 @@ const nav = navigator.userAgent;
 let mob = nav.indexOf('Mobi');
 let chrome = nav.indexOf('Chrome');
 
-let element = document.getElementsByClassName("barre");
-console.log(element);
+let element = document.getElementById("body");
 
 if ((mob > 0) && (chrome > 0)){
-  	element[0].style.setProperty("--barre", "56px");
+  	element.style.setProperty("--barre", "56px");
   	console.log('chrome');
 }
 if ((mob > 0) && (chrome < 0)){
-  	element[0].style.setProperty("--barre", "0px");
+  	element.style.setProperty("--barre", "0px");
   	console.log('default');
 }
